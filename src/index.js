8ba888e2c4541617bb0f33abb51a6888f59603fb
mg3442 <magnusprotas@gmail.com>
2018-09-30 23:33:26 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function declare(row,col) {
+    let result = [];
+    let block = {
+        row: Math.floor(row/3)*3,
+        col: Math.floor(col/3)*3,
+    };
+
+    for (let i = 0; i < 9; i++) {
+        result.push([matrix[row][i], matrix[i][col], matrix[block.row + i % 3][block.col + Math.floor(i / 3)]])
+    }
+    return result;
+}
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+        if (matrix[row][col] === 0) {
+          let declarations = declare(row, col);
+            for (let declaration of declarations) {
+                matrix[row][col] = declaration;
+                solveSudoku(matrix);
+            }  
+        }
+    }
+}
+return matrix;
 }
